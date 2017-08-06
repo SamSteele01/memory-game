5823a213787ef2a2ce95182b44f9af0f2dd6c0fb
theironyard <theironyatheironyardrd@theironyardsAir.attlocal.net>
2017-08-05 21:27:27 -0400

    Spin effect works both ways

--- main.js
+++ main.js
@@ -11,9 +11,10 @@
 // high scores - before game - play game button
 
 // For test/build. come back later and have multiple deckSizes
-// need to create a health meter - want a bar like on Outthere
+// need to create a health meter - want a bar like on Outthere-image file
 let deckSize = 24;
-let healthMeter = 10;
+let healthMeter = 24;
+let healthMeterStart = healthMeter;
 let headerItems = document.getElementById('header-items');
 let timer = document.createElement("div");
 let healthM = document.createElement("div");
@@ -32,10 +33,13 @@ timer.setAttribute("id", "timer");
 healthM.innerHTML = "Health meter " + healthMeter;
 startButton.innerHTML = "Start";
 
-let className = "";
-let headerItemLiteral = `
-<li class="${className}" id="cardDeck[i]">
-  <input type="text" name="" value="">`
+// loop-----
+// let htmlTag = div, button, select,
+let className = "header-bar";
+// let idName = timer, healthM, startButton, difficultyMenu, styleMenu, gameSize, highScores
+// let headerItemLiteral = `
+// <${htmlTag} class="${className}" id="${idName}">
+//   <input type="text" name="" value="">`;
 
 let optionName = "";
 let optionLiteral = `
@@ -48,15 +52,20 @@ let optionLiteral = `
 //
 // }
 
-// function healthMeterEnergyImagesArray() {
-  // link health values to images
+// function healthMeterEnergyImagesArray(number) {
+//   // make array of totalMismatches.length with different classNames to each target a different CSS style
+//   let arrayToReturn;
+//   for (var i = 0; i < number; i++) {
+//     []
+//   }
 // }
 
 // <div id="timer"></div>
-// <div id ="stop_timer" onclick="clearInterval(timerVar)">Stop time</div>
 
 // ------start after here-------
-// function runGame() //{wrap the rest of code?}
+// function runGame() //{wrap the rest of code?} -- no
+// setInterval(wait) get out with start button click
+// <div id ="start_timer" onclick="clearInterval(timerVar)">Start game</div>
 
 var timerVar = setInterval(countTimer, 1000);
 var totalSeconds = 0;
@@ -72,7 +81,7 @@ function countTimer() {
 
 let pairs = deckSize/2;
 let matched = 0;
-console.log(matched + ' ' + pairs);
+// console.log(matched + ' ' + pairs);
 
 function buildDeck(deckSize, pairs){
     let deck = [];
@@ -84,7 +93,7 @@ function buildDeck(deckSize, pairs){
     // console.log(deck);
     return deck;
 }
-console.log(matched + ' ' + pairs);
+// console.log(matched + ' ' + pairs);
 
 function shuffle (deckParam) {
   let i = 0;
@@ -102,11 +111,14 @@ function shuffle (deckParam) {
 let cardDeck = shuffle(buildDeck(deckSize));
 
 // celtic knots folder(15) = ../CelticKnots/
-function buildImgArray(pairs) {
-  for (var i = 0; i < pairs; i++) {
-
-  }
-}
+// let imageFolder = document.getElementsByTagName
+// holds a string = file path ex: ""../images/""
+// function buildImgArray(imageFolder, pairs) {
+//
+//   for (var i = 0; i < pairs; i++) {
+//     // images/ => random => (array.length = pairs) | array[i] = fileName
+//   }
+// }
 
 // need to assign images to cards by number. Return array of objects.
 
@@ -114,16 +126,16 @@ let cardTable = document.querySelector( "main > ul" );
 for (var i = 0; i < cardDeck.length; i++) {
   let card = document.createElement("li");
   let cardImage = document.createElement("img");
-  // let cardImage = document.createTextNode(cardDeck[i]);
   cardTable.appendChild(card);
-  // card.appendChild(cardImage);
   card.appendChild(cardImage);
-  // card.setAttribute("ID", cardDeck[i]);
-  // card.setAttribute("class", "back-side");
-  // document.getElementsByTagName('li')[i].setAttribute("background-image", "src='../CelticKnots/Celtic-knot-black.jpg'");
   cardImage.setAttribute("src", "../CelticKnots/Celtic-knot-black.jpg");
   cardImage.setAttribute("class", "back-side");
   cardImage.setAttribute("ID", cardDeck[i]);
+  // let cardImage = document.createTextNode(cardDeck[i]);
+  // card.appendChild(cardImage);
+  // card.setAttribute("ID", cardDeck[i]);
+  // card.setAttribute("class", "back-side");
+  // document.getElementsByTagName('li')[i].setAttribute("background-image", "src='../CelticKnots/Celtic-knot-black.jpg'");
   // document.locate("li img").setCSS("background-image", "../CelticKnots/"+[i]+".jpg")
 }
 
@@ -138,87 +150,112 @@ let cardLiteral = `
   <img src="${cardImg}" alt="">
 </li>`
 // -------
+//
+// let stringTargetFile = `
+// "../${imgFolder}/${pictureName}+e.target.id+${imgType}"
+// `
+// imgFolder = CelticKnots,
 
 let flippedCards = [];
-
+let flippedCardsId = [];
 let pickedCard = document.querySelector("#list-table");
-// pickedCard.addEventListener("click", flipCardFxn, true);
 pickedCard.addEventListener("click", flipCardFxn, false);
 function flipCardFxn(e) {
     if ((e.target.classList.value === "back-side") && (flippedCards.length <= 1)) {
       e.target.classList.remove("back-side");
       e.target.classList.add("visible");
-      flippedCards.push(e.target.id);
-      e.target.setAttribute("src", "../CelticKnots/Celtic-knot-"+e.target.id+".jpg");
+      flippedCards.push(e.target);
+      flippedCardsId.push(e.target.id);
+      // e.target.setAttribute("src", "../CelticKnots/Celtic-knot-"+e.target.id+".jpg");
       // lockout click listener - set to false
-      // console.log("e.target.id is "+e.target.id);
-      // changeImages(e.target);
+      console.log(e.target);
+      // spinFlipped(e.target);
+      spinFlipped(e.target);
       compareCards();
     }
 }
 
 function compareCards() {
-  if (flippedCards.length >= 2) {
-    console.log("compare");
-    if (flippedCards[0] === flippedCards[1]) {
+  // console.log(flippedCards); [img#12.visible]
+  // console.log(flippedCardsId); ["12"]
+  if (flippedCardsId.length >= 2) {
+    // console.log(e.target);
+    if (flippedCardsId[0] === flippedCardsId[1]) {
       console.log("match");
       let match = document.getElementsByClassName("visible");
       while (match.length > 0) {
         match[0].classList.add("matched");
         match[0].classList.remove("visible");
       }
+      flippedCardsId = [];
       flippedCards = [];
       matched += 1;
+
     }
     else {
       let noMatch = setTimeout(function() {
-        console.log("no match");
       let purge = document.getElementsByClassName("visible");
       while (purge.length > 0) {
-      purge[0].setAttribute("src", "../CelticKnots/Celtic-knot-black.jpg");
-      purge[0].classList.add("back-side");
-      purge[0].classList.remove("visible");
+        console.log(flippedCards[0]);
+        purge[0].classList.add("back-side");
+        purge[0].classList.remove("visible");
+        spinFlipped(flippedCards[0]);
+        flippedCards.shift()
+        // purge[0].setAttribute("src", "../CelticKnots/Celtic-knot-black.jpg");
+        // spinFlipped(purge[0]); only spins one back
+        // spinFlipped(flippedCards[0]);
       }
-      // need to target
+      flippedCardsId = [];
       flippedCards = [];
       }, 3000);
       healthMeter -= 1;
+      // document.getElementsByTagName('div').setAttribute("width", (100/healthMeterStart*healthMeter)+"%");
       healthM.innerHTML = "Health meter " + healthMeter;
       // console.log(healthMeter + "tries left");
     }
     checkWinLose(matched, pairs);
   }
-  console.log(matched + ' ' + pairs);
+  // console.log(matched + ' ' + pairs);
 }
 
-function changeImages(id) {
-  if (x.cardImage.classList.value === "visible") {
-    e.target.setAttribute("src", "../CelticKnots/Celtic-knot-"+e.target.id+".jpg");
+function changeImages(theCard) {
+  // console.log(e.target); --- uncaught ReferenceError: e is not defined
+  console.log(theCard); /*id#*/
+  if (theCard.classList.value === "visible") {
+    theCard.setAttribute("src", "../CelticKnots/Celtic-knot-"+theCard.id+".jpg");
+  }
+  // console.log(document.getElementById(theCard));
+  // console.log(document.getElementsByTagName("img"));
+  if (theCard.classList.value === "back-side") {
+  theCard.setAttribute("src", "../CelticKnots/Celtic-knot-black.jpg");
   }
 }
 
-// TODO: link to cards and images. Switch image.
-// let button = document.getElementById('box');
-// button.addEventListener("click",spinBulba);
-// console.log(button);
- function spinFlipped() {
-    console.log(this);
-    let bulba = this;
+// Ha ha ha ha ha! this is badass!!
+function spinFlipped(theCard) {
+    console.log("flipping"+theCard);
+    // let bulba = this;
     let degrees = 180;
-    let id = setInterval(frame,10);
+    let flip = setInterval(frame,10);
     function frame() {
-      if(degrees === 90);
-        // set cardImage = "img/" + imageArray[thisindex]. + ".jpg"
-        let toFlip = document.getElementsByClassName("visible");
-      else if(degrees <= 0){
-        clearInterval(id);
+      if(degrees === 90){
+        if (theCard.classList.value === "visible") {
+        theCard.setAttribute("src", "../CelticKnots/Celtic-knot-"+theCard.id+".jpg");
+        }
+        if (theCard.classList.value === "back-side") {
+        theCard.setAttribute("src", "../CelticKnots/Celtic-knot-black.jpg");
+        }
+      }
+        // let toFlip = document.getElementsByClassName("visible");
+      if(degrees <= 0){
+        clearInterval(flip);
       }
       else{
         degrees--;
-        console.log(bulba);
-        baby.style.transform = "rotateY("+degrees+"deg)"
+        console.log(theCard);
+        theCard.style.transform = "rotateY("+degrees+"deg)"
+      }
     }
-  }
 }
 
 function checkWinLose(m, p) {
@@ -226,6 +263,7 @@ function checkWinLose(m, p) {
   if (m === p) {
     // want to record time to complete -- tries left -- high scores -- points -- name?
     alert("You WIN!! \n With "+healthMeter+" moves left!");
+    // stop timer
     // enter your name
   }
   if (healthMeter === 0) {
